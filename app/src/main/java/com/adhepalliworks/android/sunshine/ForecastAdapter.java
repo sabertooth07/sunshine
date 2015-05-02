be813a8ecfbec89d3399040fcf06ba704a200b56
Raghuv <raghuv.adhepalli@outlook.com>
2015-05-02 14:29:21 -0600

    Activity 5.04

--- app/src/main/java/com/adhepalliworks/android/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/adhepalliworks/android/sunshine/ForecastAdapter.java
@@ -17,9 +17,25 @@ import com.adhepalliworks.android.sunshine.data.WeatherContract;
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
+
 
     /**
      * Prepare the weather high/lows for presentation.
@@ -50,9 +66,21 @@ public class ForecastAdapter extends CursorAdapter {
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forcast, parent, false);
 
-        return view;
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+        // TODO: Determine layoutId from viewType
+        if (viewType == VIEW_TYPE_TODAY) {
+            layoutId = R.layout.list_item_forecast_today;
+        } else if (viewType == VIEW_TYPE_FUTURE_DAY) {
+            layoutId = R.layout.list_item_forcast;
+        }
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
+
+        //View view = LayoutInflater.from(context).inflate(R.layout.list_item_forcast, parent, false);
+
+        //return view;
     }
 
     /*
